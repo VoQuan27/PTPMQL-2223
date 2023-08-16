42240aff475dc6a28cb8b9cba70bf206c8f95960
Your Name <you@example.com>
2023-08-16 09:50:29 +0700

    CreateProject

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class program 
+{
+    private static void Main(string[] args)
+    {
+        //khai bao bien kieu string va gan gia tri = Vo Hong Quan
+        string hoTen = "Vo Hong Quan";
+        //khai bao bien kieu in va gan gia tri = 18
+        int tuoi = 18;
+        Console.WriteLine("Sinh vien {0} - {1} tuoi",hoTen,tuoi);
+    }
+}
