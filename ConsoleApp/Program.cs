484c87eff837161dda09598d2530d9b59dcd08c5
Your Name <you@example.com>
2023-08-16 10:04:26 +0700

    CreateProject

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,9 +3,9 @@
     private static void Main(string[] args)
     {
         //khai bao bien kieu string va gan gia tri = Vo Hong Quan
-        string hoTen = "Vo Hong Quan";
-        //khai bao bien kieu in va gan gia tri = 18
-        int tuoi = 18;
-        Console.WriteLine("Sinh vien {0} - {1} tuoi",hoTen,tuoi);
+        const string hoTen = "Vo Hong Quan";
+        //khai bao bien kieu in va gan gia tri = 3
+        const int namLamViec = 3; 
+        Console.WriteLine("Nhan vien {0} - {1} nam kinh nghiem",hoTen,namLamViec);
     }
 }
