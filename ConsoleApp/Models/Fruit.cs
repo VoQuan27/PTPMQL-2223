bba9ac9289ba833f1bbc6fa258a68b031c339af3
Your Name <you@example.com>
2023-08-22 10:42:38 +0700

    Xay dung class Student

---
+++ ConsoleApp/Models/Fruit.cs
@@ -0,0 +1,22 @@
+namespace NewApp.Models
+{
+    public class Fruit
+    {
+        public string FruitID { get; set; }
+       public string FruitName { get; set; } 
+       public int Han { get; set; }
+       public void EnterData()
+       {
+        System.Console.Write("Ma = ");
+        FruitID = Console.ReadLine();
+        System.Console.Write("Ten  = ");
+        FruitName = Console.ReadLine();
+        System.Console.Write("HSD = ");
+        HSD = Convert.ToInt16(Console.ReadLine());
+       }
+        public void Display()
+       {
+        System.Console.WriteLine("Ma: {0} - Ten: {1} - HSD: {2} ngay",FruitID,FruitName,HSD);
+       }
+    }
+}
