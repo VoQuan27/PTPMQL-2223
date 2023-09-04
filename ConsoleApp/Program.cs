a60b2491a0325e080e8c8781332728e1ee3a17cf
Your Name <you@example.com>
2023-09-04 23:07:39 +0700

    tinh luong nhan vien

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,11 +1,5 @@
-﻿internal class program 
+﻿using ConsoleApp.Models;
+public class Program
 {
-    private static void Main(string[] args)
-    {
-        //khai bao bien kieu string va gan gia tri = Vo Hong Quan
-        const string hoTen = "Vo Hong Quan";
-        //khai bao bien kieu in va gan gia tri = 3
-        const int namLamViec = 3; 
-        Console.WriteLine("Nhan vien {0} - {1} nam kinh nghiem",hoTen,namLamViec);
-    }
+    private static void 
 }
