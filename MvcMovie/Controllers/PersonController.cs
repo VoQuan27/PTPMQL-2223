63b40cac0b3558750d00ad7e4e46a6a010ad3570
Your Name <you@example.com>
2023-10-03 10:44:50 +0700

    Excel vaf phan trang

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -1,17 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
 using MvcMovie.Models;
-
+using MvcMovie.Models.Process;
+using OfficeOpenXml;
+using X.PagedList;
 namespace MvcMovie.Controllers
 {
     public class PersonController : Controller
     {
        private readonly ApplicationDbContext _context;
+       private ExcelProcess _excelProcess = new ExcelProcess();
        public PersonController(ApplicationDbContext context)
        {
         _context = context;
        }
+              public async Task<IActionResult> Index(int? page)
+       {
+        var model = _context.Person.ToList().ToPagedList(page ?? 1, 5);
+        return View(model);
+       }
+        public async Task<IActionResult> Index(int? page, int? PageSize)
+        {
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+                new SelectListItem() { Value="3", Text= "3"},
+                new SelectListItem() { Value="5", Text= "5"},
+                new SelectListItem() { Value="10", Text= "10"},
+                new SelectListItem() { Value="15", Text= "15"},
+                new SelectListItem() { Value="25", Text= "25"},
+                new SelectListItem() { Value="50", Text= "50"},
+            };
+            int pagesize = (PageSize ?? 3);
+            ViewBag.psize = pagesize;
+            var model = _context.Person.ToList().ToPagedList(page ?? 1, pagesize);
+            return View(model);
+        }
        public async Task<IActionResult> Index()
        {
         var model = await _context.Person.ToListAsync();
@@ -109,6 +134,72 @@ await _context.SaveChangesAsync();
        private bool PersonExists(string id)
        {
         return (_context.Person?.Any(e => e.PersonID == id)).GetValueOrDefault();
+       }
+       public async Task<IActionResult> Upload()
+       {
+        return View();
+       }
+       [HttpPost]
+       [ValidateAntiForgeryToken]
+       public async Task<IActionResult> Upload(IFormFile file)
+       {
+        if (file!=null)
+        {
+            string fileExtension = Path.GetExtension(file.FileName);
+            if (fileExtension != ".xls" && fileExtension != ".xlsx")
+            {
+                ModelState.AddModelError("", "Please choose excelfile to upload!");
+            }
+            else
+            {
+                //reaname file when upload to sever
+                var fileName = DateTime.Now.ToShortTimeString() + fileExtension;
+                var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", fileName);
+                var fileLocation = new FileInfo(filePath).ToString();
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    //save file to sever
+                    await file.CopyToAsync(stream);
+                    //read data from excel file fill DataTable
+                    var dt = _excelProcess.ExcelToDataTable(fileLocation);
+                    //using for loop to read data from dt
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        //create new Person object
+                        var ps = new Person();
+                        //set value to attributes
+                        ps.PersonID = dt.Rows[i][0].ToString();
+                        ps.FullName = dt.Rows[i][1].ToString();
+                        ps.Address = dt.Rows[i][2].ToString();
+                        //add object to context
+                        _context.Add(ps);
+                    }
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+        }
+        return View();
+       }
+       public IActionResult Download()
+       {
+        //Name the file when downloading
+        var fileName = "YourFileName" + ".xlsx";
+        using(ExcelPackage excelPackage = new ExcelPackage())
+        {
+            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Sheet 1");
+            //add some text to cell A1
+            worksheet.Cells["A1"].Value = "PersonID";
+            worksheet.Cells["B1"].Value = "FullName";
+            worksheet.Cells["C1"].Value = "Address";
+            //get all Person
+            var personList = _context.Person.ToList();
+            //fill data to worksheet
+            worksheet.Cells["A2"].LoadFromCollection(personList);
+            var stream = new MemoryStream(excelPackage.GetAsByteArray());
+            //download file
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+        }
        }
 
     }
